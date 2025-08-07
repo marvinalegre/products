7e3b4f2337860d6548e6c9b5696dd2f3a129cf7f
Marvin Alegre <marvinalegredev@gmail.com>
2025-08-07 23:02:50 +0800

    chore: add products table schema

--- worker/db/schema.sql
+++ worker/db/schema.sql
@@ -7,3 +7,21 @@ CREATE TABLE users (
     hashed_password TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+    product_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    current_version_id INTEGER NOT NULL,
+    FOREIGN KEY (current_version_id) REFERENCES product_versions(version_id)
+);
+
+DROP TABLE IF EXISTS product_versions;
+CREATE TABLE product_versions (
+    version_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    product_id INTEGER NOT NULL,
+    product_name TEXT NOT NULL,
+    barcode TEXT NOT NULL,
+    price REAL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
